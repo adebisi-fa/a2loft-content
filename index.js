3158b6056c43fc5bf93d25e1c12d39198f9c521d
adebisi-fa-hp <adebisi-fa@live.com>
2018-03-26 19:18:15 +0100

    Add API build script.

---
+++ index.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var path = require('path');
+
+var rs = {};
+fs.readdirSync(path.resolve('./site/aot/pages')).forEach(collection => {
+    var entries = fs.readdirSync(
+        path.resolve(`./site/aot/pages/${collection}`)
+    );
+    rs[collection] = [];
+    entries.forEach(e => {
+        var eObj = {};
+        eObj[e] = JSON.parse(fs.readFileSync(path.resolve(`./site/aot/pages/${collection}/${e}`), 'utf8'));
+        rs[collection].push(eObj);
+    });
+    fs.writeFileSync(path.resolve(`./site/api/aot/pages/${collection}.json`), JSON.stringify(rs[collection], null, 2));
+});
+
+fs.readdirSync(path.resolve('./site/aot')).forEach(file => {
+    if (file.endsWith(".json"))
+        fs.copyFileSync(path.resolve(`./site/aot/${file}`), path.resolve(`./site/api/aot/${file}`));
+});
+
+fs.writeFileSync(path.resolve('./site/api/aot/pages/_all.json'), JSON.stringify(rs, null, 2));
